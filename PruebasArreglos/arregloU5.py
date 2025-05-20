433ab5029a1faaf2cd0412557ceb264077b622c3
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-05-20 11:35:35 -0600

    Arreglo 5

--- PruebasArreglos/arregloU5.py
+++ PruebasArreglos/arregloU5.py
@@ -1,1 +1,9 @@
-num = [56, 43, 32, 89, 67, 12, 14, 45, 29, 34]
+nume = [56, 43, 32, 89, 67, 12, 14, 45, 29, 34]
+
+conatador = 0
+
+for num in nume:
+    if num > 50:
+        print(num)
+    else:
+        print("No hay números mayores a 50")
