d5d727aba54c8187555268dd18384de9ebd3fa25
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-05-23 08:44:39 -0600

    N

--- matrices/matriz4.py
+++ matrices/matriz4.py
@@ -1,5 +1,6 @@
 precios = [[30, 23, 45], [56, 87.5, 100], [15.7, 90, 67]]
 precios_actuales = []
+sumas = []
 
 while True:
     try:
@@ -14,6 +15,13 @@ for fila in precios:
         nuevo_precio = precio * (1 + (incremento / 100))
         nueva_fila.append(round(nuevo_precio, 2))
     precios_actuales.append(nueva_fila)
+
+for fila_a, fila_b in precios, precios_actuales:
+    nueva_fila = []
+    for precio_a, precio_b in fila_a, fila_b:
+        suma = precio_a, precio_b
+        nueva_fila.append(round(suma, 2))
+    suma.append(nueva_fila)
 
 print("-"*31)
 for fila in precios:
